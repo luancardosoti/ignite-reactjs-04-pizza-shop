9e5d36ecd8791664289cea9c9b70c3d7e1cd195d
luancardosoti <luancardosoti@gmail.com>
2024-09-01 06:44:58 -0300

    feat: install shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Button } from './components/ui/button'
+import './global.css'
+
 export function App() {
   return (
-    <div>
+    <div className="w-screen h-screen bg-zinc-300">
       <h1>Hello World!</h1>
+      <Button>Click me</Button>
     </div>
   )
 }
