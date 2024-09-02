8d5fe23263832e7865bbe76e363d3656bfbea2b1
luancardosoti <luancardosoti@gmail.com>
2024-09-01 23:49:53 -0300

    feat: fix lint and add pagination component

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -3,7 +3,7 @@ import { ArrowRight, Search, X } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { TableCell, TableRow } from '@/components/ui/table'
 
-// export interface OrderTableRowProps {}
+// export type OrderTableRowProps = {}
 
 export function OrderTableRow() {
   return (
