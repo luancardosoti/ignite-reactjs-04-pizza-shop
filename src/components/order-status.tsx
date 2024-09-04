430a39946789d27fb2af9af2a57fd592326f3b2a
luancardosoti <luancardosoti@gmail.com>
2024-09-04 12:05:39 -0300

    feat: add order list with api

---
+++ src/components/order-status.tsx
@@ -0,0 +1,38 @@
+import clsx from 'clsx'
+
+type OrderStatus =
+  | 'pending'
+  | 'canceled'
+  | 'processing'
+  | 'delivering'
+  | 'delivered'
+
+type OrderStatusProps = {
+  status: OrderStatus
+}
+
+const orderStatusMap: Record<OrderStatus, string> = {
+  pending: 'Pendente',
+  canceled: 'Cancelado',
+  delivering: 'Em entrega',
+  processing: 'Em preparo',
+  delivered: 'Entregue',
+}
+
+export function OrderStatus({ status }: OrderStatusProps) {
+  return (
+    <div className="flex items-center gap-2">
+      <span
+        className={clsx('h-2 w-2 rounded-full', {
+          'bg-slate-500': status === 'pending',
+          'bg-rose-500': status === 'canceled',
+          'bg-emerald-500': status === 'delivered',
+          'bg-amber-500': status === 'delivering' || status === 'processing',
+        })}
+      />
+      <span className="font-medium text-muted-foreground">
+        {orderStatusMap[status]}
+      </span>
+    </div>
+  )
+}
