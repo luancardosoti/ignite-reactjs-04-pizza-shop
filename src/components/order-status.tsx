a338ee62dbf05b7405a21c1a7966eeb5208b231c
luancardosoti <luancardosoti@gmail.com>
2024-09-07 16:39:36 -0300

    test: add test do OrderStatus component

--- src/components/order-status.tsx
+++ src/components/order-status.tsx
@@ -23,8 +23,9 @@ export function OrderStatus({ status }: OrderStatusProps) {
   return (
     <div className="flex items-center gap-2">
       <span
+        data-testid="badge"
         className={clsx('h-2 w-2 rounded-full', {
-          'bg-slate-500': status === 'pending',
+          'bg-slate-400': status === 'pending',
           'bg-rose-500': status === 'canceled',
           'bg-emerald-500': status === 'delivered',
           'bg-amber-500': status === 'delivering' || status === 'processing',
